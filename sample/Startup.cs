bc66df4e288f087c67a7c30e9f2c550747e1519b
沈伟 <330589465@qq.com>
2020-05-10 23:10:20 +0800

    .

--- sample/Startup.cs
+++ sample/Startup.cs
@@ -16,6 +16,7 @@ namespace sample
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,13 +29,7 @@ namespace sample
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
         }
     }
 }
