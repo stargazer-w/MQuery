45c822de712d7c57b05d8772ffffbadd2207ab45
stargazer-w <330589465@qq.com>
2022-06-20 17:35:54 +0800

    提供接口类型的绑定，严格NRT

--- src/MQuery.AspNetCore/MvcOptionsExtenssions.cs
+++ src/MQuery.AspNetCore/MvcOptionsExtenssions.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using MQuery.QueryString;
 
@@ -8,7 +6,7 @@ namespace MQuery.AspNetCore
 {
     public static class MvcOptionsExtenssions
     {
-        public static MvcOptions AddMQuery(this MvcOptions mvc, Action<ParserOptions> optionsBuilder = null)
+        public static MvcOptions AddMQuery(this MvcOptions mvc, Action<ParserOptions>? optionsBuilder = null)
         {
             var options = new ParserOptions();
             optionsBuilder?.Invoke(options);
