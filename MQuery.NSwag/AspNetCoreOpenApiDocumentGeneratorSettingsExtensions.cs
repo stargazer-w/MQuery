064ae291f4fe155202e4bdd3a0fae949f9603b94
stargazer-w <330589465@qq.com>
2022-06-27 10:33:42 +0800

    添加对NSwag的支持

---
+++ MQuery.NSwag/AspNetCoreOpenApiDocumentGeneratorSettingsExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NSwag.Generation.AspNetCore;
+
+namespace MQuery.NSwag
+{
+    public static class AspNetCoreOpenApiDocumentGeneratorSettingsExtensions
+    {
+        public static void AddMQuery(this AspNetCoreOpenApiDocumentGeneratorSettings settings)
+        {
+            if(settings is null)
+            {
+                throw new ArgumentNullException(nameof(settings));
+            }
+
+            var processor = new MQueryOpenApiDocumentProcessor();
+            settings.OperationProcessors.Add(processor);
+            settings.PostProcess = processor.OpenApiDocumentPostProcess;
+        }
+    }
+}
