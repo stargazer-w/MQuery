48ad0910a32e4632be2ab854dd7576e399e0047c
沈伟 <shenwei4002@dingtalk.com>
2022-07-08 10:18:01 +0800

    添加对文档直接操作的帮助函数

---
+++ src/MQuery/Expressions/FilterExtensions.cs
@@ -0,0 +1,21 @@
+﻿using MQuery.Filter;
+
+namespace MQuery.Expressions
+{
+    public static class FilterExtensions
+    {
+        public static FilterDocument<T> And<T>(this FilterDocument<T> @this, IParameterOperation operation)
+        {
+            if(@this.Operation is And and)
+            {
+                and.Operators.Add(operation);
+            }
+            else
+            {
+                and = new And(@this.Operation, operation);
+                @this.Operation = and;
+            }
+            return @this;
+        }
+    }
+}
