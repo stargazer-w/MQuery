a8cb75b580ef07b255f57c95b84ac4ed870afb45
stargazer-w <330589465@qq.com>
2022-07-08 10:18:42 +0800

    新增ne(not equal)操作

---
+++ src/MQuery/Filter/NotEqual.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+
+namespace MQuery.Filter
+{
+    public class NotEqual<T> : IOperator
+    {
+        public T? Value { get; }
+
+        public NotEqual(T? value)
+        {
+            Value = value;
+        }
+
+        public Expression Combine(Expression left)
+        {
+            var constValue = Expression.Constant(Value, typeof(T));
+            return Expression.NotEqual(left, constValue);
+        }
+    }
+}
