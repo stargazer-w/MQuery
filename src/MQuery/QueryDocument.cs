f0aea2c9752b4b3f1ad5aa0586a3e2b2d8404679
沈伟 <shenwei4002@dingtalk.com>
2022-07-12 16:25:19 +0800

    重写根文档

--- src/MQuery/QueryDocument.cs
+++ src/MQuery/QueryDocument.cs
@@ -1,4 +1,7 @@
-﻿using MQuery.Filter;
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using MQuery.Filter;
 using MQuery.Slice;
 using MQuery.Sort;
 
@@ -11,5 +14,16 @@ namespace MQuery
         public SortDocument<T> Sort { get; } = new();
 
         public SliceDocument Slice { get; } = new();
+
+        public Expression<Func<IQueryable<T>, IQueryable<T>>> ToExpression()
+        {
+            var filterExpr = Filter.ToExpression();
+            var sortExpr = Sort.ToExpression();
+            var sliceExpr = Slice.ToExpression<T>();
+
+            var filterThenSort = Expression.Invoke(sortExpr, Expression.Invoke(filterExpr, filterExpr.Parameters));
+            var filterThenSortThenSlice = Expression.Invoke(sliceExpr, filterThenSort);
+            return Expression.Lambda<Func<IQueryable<T>, IQueryable<T>>>(filterThenSortThenSlice, filterExpr.Parameters);
+        }
     }
 }
