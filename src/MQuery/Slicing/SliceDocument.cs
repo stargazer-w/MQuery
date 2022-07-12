2e397cbede7bd5f1b83a2bf7abb1829c034b7c27
沈伟 <shenwei4002@dingtalk.com>
2022-07-12 16:24:52 +0800

    重写slice

--- src/MQuery/Slicing/SliceDocument.cs
+++ src/MQuery/Slicing/SliceDocument.cs
@@ -1,9 +1,24 @@
-﻿namespace MQuery.Slice
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace MQuery.Slice
 {
     public class SliceDocument
     {
         public int? Skip { get; set; }
 
         public int? Limit { get; set; }
+
+        public Expression<Func<IQueryable<T>, IQueryable<T>>> ToExpression<T>()
+        {
+            return this switch
+            {
+                { Skip: null, Limit: null } => qy => qy,
+                { Skip: null, Limit: int limit } => qy => qy.Take(limit),
+                { Skip: int skip, Limit: null } => qy => qy.Skip(skip),
+                { Skip: int skip, Limit: int limit } => qy => qy.Skip(skip).Take(limit),
+            };
+        }
     }
 }
