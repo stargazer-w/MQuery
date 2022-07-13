03467e48067e0ceb525185ef74e89916c5c95a55
沈伟 <shenwei4002@dingtalk.com>
2022-07-13 10:59:27 +0800

    补充Any测试

--- test/MQuery.Tests/Filter/AnyTests.cs
+++ test/MQuery.Tests/Filter/AnyTests.cs
@@ -34,5 +34,18 @@ namespace MQuery.Filter.Tests
 
             Assert.That(func(), Is.EqualTo(result));
         }
+
+        [TestCase(true, false)]
+        [TestCase(false, false)]
+        [Test()]
+        public void CombineTestWithNull(bool condition, bool result)
+        {
+            var propOp = PropertyOperation<object>.Self(new ConstantOperator(condition));
+            var op = new Any(propOp);
+            var body = op.Combine(Expression.Constant(null, typeof(object).MakeArrayType()));
+            var func = Expression.Lambda<Func<bool>>(body).Compile();
+
+            Assert.That(func(), Is.EqualTo(result));
+        }
     }
 }
