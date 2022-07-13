7ea87057f3c70164dc5b4ef9cd1deeb5ca5b20cd
沈伟 <shenwei4002@dingtalk.com>
2022-07-13 10:59:41 +0800

    添加All操作符

---
+++ test/MQuery.Tests/Filter/AllTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq.Expressions;
+using NUnit.Framework;
+
+namespace MQuery.Filter.Tests
+{
+    [TestFixture()]
+    public class AllTests
+    {
+        public class ConstantOperator : IOperator
+        {
+            private readonly object _value;
+
+            public ConstantOperator(object value)
+            {
+                _value = value;
+            }
+
+            public Expression Combine(Expression left)
+            {
+                return Expression.Constant(_value);
+            }
+        }
+
+        [TestCase(true, true)]
+        [TestCase(false, false)]
+        [Test()]
+        public void CombineTest(bool condition, bool result)
+        {
+            var propOp = PropertyOperation<object>.Self(new ConstantOperator(condition));
+            var op = new All(propOp);
+            var body = op.Combine(Expression.Constant(new object[] { null }));
+            var func = Expression.Lambda<Func<bool>>(body).Compile();
+
+            Assert.That(func(), Is.EqualTo(result));
+        }
+
+        [TestCase(true, true)]
+        [TestCase(false, true)]
+        [Test()]
+        public void CombineTestWithNull(bool condition, bool result)
+        {
+            var propOp = PropertyOperation<object>.Self(new ConstantOperator(condition));
+            var op = new All(propOp);
+            var body = op.Combine(Expression.Constant(null, typeof(object).MakeArrayType()));
+            var func = Expression.Lambda<Func<bool>>(body).Compile();
+
+            Assert.That(func(), Is.EqualTo(result));
+        }
+    }
+}
