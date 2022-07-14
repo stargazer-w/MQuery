9479a9a0fcb271b08d80a100079bae557db5b36b
沈伟 <shenwei4002@dingtalk.com>
2022-07-14 15:11:40 +0800

    新增or

--- src/MQuery/Extensions/FilterDocumentExtensions.cs
+++ src/MQuery/Extensions/FilterDocumentExtensions.cs
@@ -30,5 +30,30 @@ namespace MQuery.Extensions
             }
             return filter;
         }
+
+        public static FilterDocument<T> Or<T, U>(this FilterDocument<T> filter, Expression<Func<T, U>> selector, IOperator @operator)
+        {
+            var operation = new PropertyOperation<T>(new PropertyLambda<T>(selector), @operator);
+            return filter.Or(operation);
+        }
+
+        public static FilterDocument<T> Or<T>(this FilterDocument<T> filter, Expression<Func<T, bool>> expression)
+        {
+            var operation = new LambdaOperation<T>(expression);
+            return filter.Or(operation);
+        }
+
+        public static FilterDocument<T> Or<T>(this FilterDocument<T> filter, IParameterOperation operation)
+        {
+            if(filter.Operation is Or or)
+            {
+                or.Operations.Add(operation);
+            }
+            else
+            {
+                filter.Operation = new And(filter.Operation, operation);
+            }
+            return filter;
+        }
     }
 }
