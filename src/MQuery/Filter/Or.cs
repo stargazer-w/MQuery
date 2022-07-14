72a012bcd93a97f494e59c73547fa8eb42763428
stargazer-w <330589465@qq.com>
2022-07-14 15:11:40 +0800

    新增or

---
+++ src/MQuery/Filter/Or.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace MQuery.Filter
+{
+    public class Or : IParameterOperation
+    {
+        public IList<IParameterOperation> Operations { get; }
+
+        public Or(params IParameterOperation[] operations)
+        {
+            Operations = operations.ToList();
+        }
+
+        public Expression Combine(ParameterExpression left)
+        {
+            var first = Operations.FirstOrDefault();
+            if(first == null)
+                return Expression.Constant(true);
+            return Operations.Skip(1)
+                 .Aggregate(
+                     first.Combine(left),
+                     (x, y) => Expression.OrElse(x, y.Combine(left))
+                 );
+        }
+    }
+}
---
+++ test/MQuery.Tests/Filter/OrTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using NUnit.Framework;
+using Shouldly;
+
+namespace MQuery.Filter.Tests
+{
+    [TestFixture()]
+    public class OrTests
+    {
+        [TestCase(new[] { true, true }, true)]
+        [TestCase(new[] { false, false }, false)]
+        [TestCase(new[] { true, false }, true)]
+        [Test()]
+        public void CombineTest(bool[] conditions, bool result)
+        {
+            var propOps = conditions
+                .Select(x => new LambdaOperation<object>(_ => x))
+                .ToArray();
+            var op = new Or(propOps);
+            var param = Expression.Parameter(typeof(object));
+            var body = op.Combine(param);
+            var func = Expression.Lambda<Func<bool>>(body).Compile();
+
+            func().ShouldBe(result);
+        }
+
+    }
+}
