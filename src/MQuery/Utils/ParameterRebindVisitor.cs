3f0e7f8a65e1cb1bf665c2b169edcae5e811d4be
沈伟 <shenwei4002@dingtalk.com>
2022-07-14 11:01:24 +0800

    重写PropertySelector相关

---
+++ src/MQuery/Utils/ParameterRebindVisitor.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+
+namespace MQuery.Utils
+{
+    internal class ParameterRebindVisitor : ExpressionVisitor
+    {
+        private readonly ParameterExpression _parameter;
+
+        public ParameterRebindVisitor(ParameterExpression parameter)
+        {
+            _parameter = parameter;
+        }
+
+        protected override Expression VisitParameter(ParameterExpression node)
+        {
+            return _parameter;
+        }
+    }
+}
